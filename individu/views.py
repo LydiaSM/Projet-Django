08dd47aba46e4e679aa9463f75b8b71ac3846a19
Smahi <l.smahi@hotmail.fr>
2021-01-08 00:35:21 +0100

    Afficher bd sur la premiere page

--- individu/views.py
+++ individu/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Individu
 
 # Create your views here.
 
 def individu_list(request):
-    return render(request, 'home/individu_list.html', {})
+    individus= Individu.objects.all()
+    return render(request, 'home/individu_list.html', {'individus':individus})
