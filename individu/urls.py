aa939ec36b6590cb9eec8afc490b3cd293f058ad
Smahi <l.smahi@hotmail.fr>
2021-01-08 20:19:14 +0100

    Ajout de la page detail de l'individu

--- individu/urls.py
+++ individu/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.individu_list, name='individu_list'),
+    path('individu/<int:pk>/', views.Action, name='Action'),
 ]
--- individu/views.py
+++ individu/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Individu
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 
 def individu_list(request):
     individus= Individu.objects.all()
-    return render(request, 'home/individu_list.html', {'individus':individus})
+    return render(request, 'home/individu_list.html', {'individus': individus})
+
+def Action(request, pk):
+    individu = get_object_or_404(Individu, pk=pk)
+    return render(request, 'home/Action.html', {'individu': individu})
